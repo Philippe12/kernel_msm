KonstaT/android_kernel_nokia_msm8x25

msm: board-msm7627a-io: Add key support for EVBD+
Configure gpios and add platform data for gpio matrix
driver to support keys on EVBD+ platform.

(cherry picked from commit 5f9469409a029eb11c1b73a2630bc512f2e8221f)

Change-Id: Idfb4fe1d81b18556e80bc55f214faad7d0c340ad
CRs-fixed: 434294
Signed-off-by: Mohan Pallaka <mpallaka@codeaurora.org>

arch/arm/mach-msm/board-msm7627a-io.c
@@ -27,8 +27,9 @@
 #include <asm/mach-types.h>
 #include <mach/rpc_server_handset.h>
 #include <mach/pmic.h>
-
+#include <mach/socinfo.h>
 #include "devices.h"
+
 #include "board-msm7627a.h"
 #include "devices-msm7x2xa.h"
 
@@ -751,6 +752,9 @@ static const unsigned short keymap_sku3[] = {
 static unsigned int kp_row_gpios_skud[] = {31, 32};
 static unsigned int kp_col_gpios_skud[] = {37};
 
+static unsigned int kp_row_gpios_evbdp[] = {42, 37};
+static unsigned int kp_col_gpios_evbdp[] = {31};
+
 static const unsigned short keymap_skud[] = {
 	[KP_INDEX_SKU3(0, 0)] = KEY_VOLUMEUP,
 	[KP_INDEX_SKU3(0, 1)] = KEY_VOLUMEDOWN,
@@ -929,6 +933,23 @@ void __init qrd7627a_add_io_devices(void)
 		kp_matrix_info_sku3.input_gpios = kp_col_gpios_skud;
 		kp_matrix_info_sku3.noutputs = ARRAY_SIZE(kp_row_gpios_skud);
 		kp_matrix_info_sku3.ninputs = ARRAY_SIZE(kp_col_gpios_skud);
+		/* keypad info for EVBD+ */
+		if (machine_is_msm8625q_evbd() &&
+			(socinfo_get_platform_type() == 13)) {
+			gpio_tlmm_config(GPIO_CFG(37, 0,
+						GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN,
+						GPIO_CFG_8MA), GPIO_CFG_ENABLE);
+			gpio_tlmm_config(GPIO_CFG(42, 0,
+						GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN,
+						GPIO_CFG_8MA), GPIO_CFG_ENABLE);
+			gpio_tlmm_config(GPIO_CFG(31, 0,
+						GPIO_CFG_INPUT, GPIO_CFG_PULL_UP,
+						GPIO_CFG_8MA), GPIO_CFG_ENABLE);
+			kp_matrix_info_sku3.output_gpios = kp_row_gpios_evbdp;
+			kp_matrix_info_sku3.input_gpios = kp_col_gpios_evbdp;
+			kp_matrix_info_sku3.noutputs = ARRAY_SIZE(kp_row_gpios_evbdp);
+			kp_matrix_info_sku3.ninputs = ARRAY_SIZE(kp_col_gpios_evbdp);
+		}
 	}
 
 	if (machine_is_msm8625_evt())

Philippe12/kernel_msm

(cherry picked from commit 1c8d3c47ee1b5c94206c2cb22c96f42b9e7ab0e8)
Conflicts:
	arch/arm/mach-msm/board-msm7627a-io.c

arch/arm/mach-msm/board-msm7627a-io.c
@@ -28,8 +28,8 @@
 #include <mach/rpc_server_handset.h>
 #include <mach/pmic.h>
 #include <mach/socinfo.h>
-
 #include "devices.h"
+
 #include "board-msm7627a.h"
 #include "devices-msm7x2xa.h"
 
@@ -1070,6 +1070,9 @@ static struct platform_device msm_device_tricolor_leds = {
 static unsigned int kp_row_gpios_qrd3[] = {31, 32};
 static unsigned int kp_col_gpios_qrd3[] = {36, 37};
 
+static unsigned int kp_row_gpios_evbdp[] = {42, 37};
+static unsigned int kp_col_gpios_evbdp[] = {31};
+
 static const unsigned short keymap_qrd3[] = {
 	[KP_INDEX_SKU3(0, 0)] = KEY_VOLUMEUP,
 	[KP_INDEX_SKU3(0, 1)] = KEY_VOLUMEDOWN,
@@ -1274,6 +1277,24 @@ void __init qrd7627a_add_io_devices(void)
 	/* keypad */
 	if (machine_is_msm8625_qrd5() || machine_is_msm7x27a_qrd5a())
 		kp_matrix_info_8625.keymap = keymap_8625_qrd5;
+	/* keypad info for EVBD+ */
+	if (machine_is_msm8625q_evbd() &&
+			(socinfo_get_platform_type() == 13)) {
+			gpio_tlmm_config(GPIO_CFG(37, 0,
+						GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN,
+						GPIO_CFG_8MA), GPIO_CFG_ENABLE);
+			gpio_tlmm_config(GPIO_CFG(42, 0,
+						GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN,
+						GPIO_CFG_8MA), GPIO_CFG_ENABLE);
+			gpio_tlmm_config(GPIO_CFG(31, 0,
+						GPIO_CFG_INPUT, GPIO_CFG_PULL_UP,
+						GPIO_CFG_8MA), GPIO_CFG_ENABLE);
+			kp_matrix_info_skud.output_gpios = kp_row_gpios_evbdp;
+			kp_matrix_info_skud.input_gpios = kp_col_gpios_evbdp;
+			kp_matrix_info_skud.noutputs = ARRAY_SIZE(kp_row_gpios_evbdp);
+			kp_matrix_info_skud.ninputs = ARRAY_SIZE(kp_col_gpios_evbdp);
+	}
+
 	if (machine_is_msm7627a_evb() || machine_is_msm8625_evb() ||
 			machine_is_msm8625_qrd5() || machine_is_msm7x27a_qrd5a())
 		platform_device_register(&kp_pdev_8625);