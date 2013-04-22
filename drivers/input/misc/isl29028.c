9d3215b8e900ed0a40c87d43c205c48021553e1f
pfang <fangpan@codeaurora.org>
2013-04-22 16:08:59 +0800

    CAF:QRD_BSP:SENSORS:None:467133:Fix the CR Sensors leads another 4-6mA during GSM voice call, 13mA during CDMA voice call
    	Component Name: SENSORS
    	Feature Name: None
    	Fixed CRs: 467133
    	Targets: msm8X25_skuD
    	Required VUs:
    	Dependency Notes:
    	Limitations:
    	Suggested Testing: test
    	Peer Review:
    	Reference Build or VU:
    	Comments:
    	(*commit tool - 1.2.0)

--- drivers/input/misc/isl29028.c
+++ drivers/input/misc/isl29028.c
@@ -34,6 +34,12 @@
 #include <linux/semaphore.h>
 #include <linux/sysfs.h>
 #include <linux/mutex.h>
+#include <linux/irq.h>
+#include <linux/miscdevice.h>
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <linux/earlysuspend.h>
+#include <linux/input/isl29028.h>
 
 
 #define ISL29028_MODULE_NAME	"isl29028"
@@ -50,7 +56,7 @@
 #else
 #define CONFIG_VAL	0x00
 #endif
-#define INTERRUPT_VAL	0x00
+#define INTERRUPT_VAL	0x20
 
 //isl29028
 #define ISL29028_CONFIG		0x01
@@ -93,7 +99,7 @@
 #define CURRENT_100MA (100 <<8)
 #define CURRENT_200MA (200 <<8)
 #define PROXIMITY_MAX 140
-#define THRESHOLD	50
+//#define THRESHOLD	50
 #define STEP		15
 #define ADJ_DBG
 #define NUM  5
@@ -115,20 +121,25 @@ struct isl29028_data{
 	struct input_dev *proximity_input_dev;
 	struct input_dev *light_input_dev;
 	struct delayed_work als_work;
-	struct delayed_work prox_work;
+	struct work_struct prox_work;
 	int prox_interval;
 	int als_interval;
 	struct wake_lock prox_wake_lock;
 	atomic_t als_working;
 	atomic_t prox_working;
 	atomic_t prox_locking;
-} *data;
+	struct isl29028_platform_data *pdata;
+	bool  ps_near;
+};
 
 static struct workqueue_struct *wq;
 static struct mutex prox_lock;
 static struct mutex als_lock;
 
-static int threshold = THRESHOLD;
+static int prox_threshold_hi = 50;	//Default value
+static int prox_threshold_lo = 30;
+
+//static int threshold = THRESHOLD;
 
 /* set 3 level light sensor level*/
 #define ISL_LUX_LEVELS 3
@@ -187,6 +198,12 @@ static int isl29028_enable_prox(struct i2c_client *client)
 {
 	u8 temp;
 	int ret = 0;
+	char buf[2] = {0};
+
+	buf[0] = prox_threshold_hi & 0xff;
+	buf[1] = prox_threshold_lo & 0xff;
+	isl29028_write_reg(client,ISL29028_PROX_LT,buf[1]);
+	isl29028_write_reg(client,ISL29028_PROX_HT,buf[0]);
 
 	ret |= isl29028_read_reg(client, ISL29028_CONFIG, &temp);
 	temp|= PROX_EN;
@@ -228,7 +245,7 @@ static int isl29028_disable_als(struct i2c_client *client)
 	return ret;
 }
 
-static void sysfs_enable_prox(int enable)
+static void sysfs_enable_prox(int enable,struct isl29028_data *data)
 {
 	if(enable){
 		if(!atomic_read(&data->prox_working)){
@@ -242,7 +259,7 @@ static void sysfs_enable_prox(int enable)
 			isl29028_enable_prox(data->client);
 			pre_prox_cm = -1;
 			/*delay for 20ms to makesure prox enable*/
-			queue_delayed_work(wq, &data->prox_work,20);
+//			queue_delayed_work(wq, &data->prox_work,20);
 		}else{
 			PR_DEB("Prox thread has running\n");
 		}	
@@ -255,14 +272,14 @@ static void sysfs_enable_prox(int enable)
 			PR_DEB("SYSFS_DIS_PROX and unlocked prox_locking=%d\n",
 				atomic_read(&data->prox_locking));
 
-			cancel_delayed_work_sync(&data->prox_work);
+			cancel_work_sync(&data->prox_work);
 			isl29028_disable_prox(data->client);
 		}else{
 			PR_DEB("Prox thread has not running already\n");
 		}	
 	}
 }
-static void sysfs_enable_als(int enable)
+static void sysfs_enable_als(int enable,struct isl29028_data *data)
 {
 	if(enable){
 		if(!atomic_read(&data->als_working)){
@@ -317,6 +334,7 @@ static ssize_t light_enable_store(struct device *dev,
 				struct device_attribute *attr,
 				const char *buf, size_t size)
 {
+	struct isl29028_data *data = dev_get_drvdata(dev);
 	int new_value;
 	if (sysfs_streq(buf, "1"))
 		new_value = 1;
@@ -328,7 +346,7 @@ static ssize_t light_enable_store(struct device *dev,
 	}
 	PR_DEB("SYSFS_EN_ALS %d\n", new_value);
 	mutex_lock(&als_lock);
-	sysfs_enable_als(new_value);
+	sysfs_enable_als(new_value,data);
 	mutex_unlock(&als_lock);
 	return size;
 }
@@ -337,7 +355,9 @@ static ssize_t proximity_enable_store(struct device *dev,
 				struct device_attribute *attr,
 				const char *buf, size_t size)
 {
+	struct isl29028_data *data = dev_get_drvdata(dev);
 	int new_value;
+	u8 int_flag;
 	if (sysfs_streq(buf, "1"))
 		new_value = 1;
 	else if (sysfs_streq(buf, "0"))
@@ -348,7 +368,8 @@ static ssize_t proximity_enable_store(struct device *dev,
 	}
 	PR_DEB("SYSFS_EN_PROX %d\n", new_value);
 	mutex_lock(&prox_lock);
-	sysfs_enable_prox(new_value);
+	isl29028_read_reg(data->client,ISL29028_INTCONFIG,&int_flag);
+	sysfs_enable_prox(new_value,data);
 	mutex_unlock(&prox_lock);
 	return size;
 }
@@ -390,28 +411,62 @@ static int isl29028_init_client(struct i2c_client *client)
 
 static void prox_work_func(struct work_struct *work)
 {
-	int cm;
+	int cm = 0;
 	u8 val;
-	struct isl29028_data *data = container_of(work,struct isl29028_data,prox_work.work);
+	struct isl29028_data *data = container_of(work,struct isl29028_data,prox_work);
 
+retry:
 	isl29028_prox_read(data->client, &val);
-	PR_DEB("############threshold =%d,prox_data val= %d\n",threshold ,val);
-	/* 0 is close, 1 is far */
-	if(val > threshold)
-		cm = 0;
-	else
-		cm = 1;
-
-	if(pre_prox_cm != cm)
-	{
-		PR_DEB("############prox_data cm = %d\n",cm);
+	if(val < prox_threshold_lo){  //far
+		cm = 2;
+		data->ps_near = false;
+		isl29028_write_reg(data->client,ISL29028_PROX_LT,0);
+		isl29028_write_reg(data->client,ISL29028_PROX_LT,prox_threshold_hi & 0xff);
 		input_report_abs(data->proximity_input_dev, ABS_DISTANCE, cm);
 		input_sync(data->proximity_input_dev);
+	}else if (val > prox_threshold_hi){ //near
+		cm = 0;
+		data->ps_near = true;
+		isl29028_write_reg(data->client,ISL29028_PROX_HT,0xff);
+		isl29028_write_reg(data->client,ISL29028_PROX_LT,prox_threshold_lo & 0xff);
+		input_report_abs(data->proximity_input_dev, ABS_DISTANCE, cm);
+		input_sync(data->proximity_input_dev);
+		msleep(500);
+		goto retry;
 	}
+	else{
+		isl29028_write_reg(data->client,ISL29028_PROX_HT,0xff);
+		isl29028_write_reg(data->client,ISL29028_PROX_LT,0x00);
+		msleep(500);
+		goto retry;
+	}
+
+	PR_DEB("############prox_data cm = %d\n",cm);
 	pre_prox_cm = cm;
-	PR_DEB("-ISL29028-prox-msecs=%d\n",(int)msecs_to_jiffies(data->prox_interval));
-	queue_delayed_work(wq, &data->prox_work,msecs_to_jiffies(data->prox_interval));	
+	isl29028_enable_prox(data->client);
+//	queue_work(wq, &data->prox_work);
 }
+
+/* this is ISR*/
+static irqreturn_t isl29028_interrupt(int vec, void *info)
+{
+	struct i2c_client *client=(struct i2c_client *)info;
+	struct isl29028_data *data = i2c_get_clientdata(client);
+	u8 int_flag;
+
+	wake_lock(&data->prox_wake_lock);
+	prox_work_func(&data->prox_work);
+	isl29028_read_reg(client,ISL29028_INTCONFIG,&int_flag);
+	if(int_flag & PROX_FLAG){
+		int_flag &=~(PROX_FLAG);
+		int_flag &=~(ALS_FLAG);
+		isl29028_write_reg(client,ISL29028_INTCONFIG,int_flag);
+	}
+	wake_unlock(&data->prox_wake_lock);
+
+	return IRQ_HANDLED;
+}
+
 static int to_lux(struct i2c_client *client,u16 data)
 {
 	int res = 0;
@@ -481,16 +536,24 @@ static int isl29028_reset(struct i2c_client *client)
 static int __devinit isl29028_probe(struct i2c_client *client,
 				   const struct i2c_device_id *id)
 {
-	int err = 0;
+	int err = -ENODEV;
 	struct input_dev *input_dev;
+	struct isl29028_data *data;
 	PR_DEB("isl29028_probe\n");
+
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
+		return err;
+	}
 
 	data = kzalloc(sizeof(struct isl29028_data), GFP_KERNEL);
 	if (data == NULL) {
 		err = -ENOMEM;
 		goto exit;
 	}
+
+	data->pdata = client->dev.platform_data;
 	data->client = client;
+	data->ps_near = false;
 	i2c_set_clientdata(client, data);
 
 	dev_info(&client->dev, "support ver. %s enabled\n", DRIVER_VERSION);
@@ -553,7 +616,7 @@ static int __devinit isl29028_probe(struct i2c_client *client,
 		err = -1;
 		goto exit4;
 	}
-	INIT_DELAYED_WORK(&data->prox_work, prox_work_func);
+	INIT_WORK(&data->prox_work, prox_work_func);
 	INIT_DELAYED_WORK(&data->als_work, als_work_func);
 
 	/*isl29028 reset*/
@@ -563,12 +626,20 @@ static int __devinit isl29028_probe(struct i2c_client *client,
 	if(err)
 		goto exit5;
 
+	wake_lock_init(&data->prox_wake_lock, WAKE_LOCK_SUSPEND, "prox_wake_lock");
+	/* setup interrupt and wakeup */
+	device_init_wakeup(&client->dev, true);
+	err = request_threaded_irq(data->pdata->int_gpio, NULL, isl29028_interrupt, IRQ_TYPE_EDGE_FALLING, ISL29028_MODULE_NAME, (void *)client);
+	if(err){
+		pr_err("%s Could not allocate irq(%d) !\n", __func__, data->pdata->int_gpio);
+		goto exit_irq;
+	}
+
 	/*init mutex for prox */
         mutex_init(&prox_lock);
 	/*init mutex for als */
         mutex_init(&als_lock);
 
-	wake_lock_init(&data->prox_wake_lock, WAKE_LOCK_SUSPEND, "prox_wake_lock");
 	/*set init state to not-working*/
 	atomic_set(&data->als_working,0);
 	atomic_set(&data->prox_working,0);
@@ -582,6 +653,9 @@ static int __devinit isl29028_probe(struct i2c_client *client,
 	dev_info(&client->dev, "sensor driver probe successful\n");
 	goto exit;
 
+exit_irq:
+	device_init_wakeup(&client->dev, false);
+	wake_lock_destroy(&data->prox_wake_lock);
 exit5:
 	destroy_workqueue(wq);
 exit4:
@@ -606,9 +680,12 @@ static int __devexit isl29028_remove(struct i2c_client *client)
 
 	struct isl29028_data *data = i2c_get_clientdata(client);
 
+	free_irq(data->pdata->int_gpio,(void *)client);
+	device_init_wakeup(&client->dev,false);
+
 	if(atomic_read(&data->prox_working)){
 		isl29028_disable_prox(data->client);
-		cancel_delayed_work_sync(&data->prox_work);
+		cancel_work_sync(&data->prox_work);
 	}
 
 	if(atomic_read(&data->als_working)){
@@ -636,16 +713,26 @@ static int __devexit isl29028_remove(struct i2c_client *client)
 static int isl29028_suspend(struct i2c_client *client, pm_message_t mesg)
 {
 	int ret = 0,prox_s,als_s;
+	u8 status;
 	struct isl29028_data *data = i2c_get_clientdata(client);
 
 	prox_s = atomic_read(&data->prox_working);
 	als_s = atomic_read(&data->als_working);
+	isl29028_read_reg(client,ISL29028_CONFIG,&status);
+
+	if(status & ALS_EN){
+		isl29028_disable_als(data->client);
+	}
+
+	if(status & PROX_EN){
+		enable_irq_wake(data->pdata->int_gpio);
+	}
 
 	PR_DEB("Enter %s:prox_state:%d,als_state:%d\n",
 		__func__,prox_s,als_s);
 
 	if(prox_s){
-		cancel_delayed_work_sync(&data->prox_work);
+		cancel_work_sync(&data->prox_work);
 		isl29028_disable_prox(data->client);	
 	}
 	if(als_s){
@@ -659,19 +746,30 @@ static int isl29028_suspend(struct i2c_client *client, pm_message_t mesg)
 static int isl29028_resume(struct i2c_client *client)
 {
 	int ret = 0,prox_s,als_s;
+	u8 status;
 	struct isl29028_data *data = i2c_get_clientdata(client);
 	prox_s = atomic_read(&data->prox_working);
 	als_s = atomic_read(&data->als_working);
+
+	isl29028_read_reg(client,ISL29028_CONFIG,&status);
+
+	if(status & ALS_EN){
+		isl29028_enable_als(data->client);
+	}
+
+	if(status & PROX_EN){
+		disable_irq_wake(data->pdata->int_gpio);
+	}
 
 	PR_DEB("Enter %s:prox_state:%d,als_state:%d\n",
 		__func__,prox_s,als_s);
 
 	if(prox_s){
 		isl29028_enable_prox(data->client);
-		queue_delayed_work(wq, &data->prox_work,20);
+//		queue_delayed_work(wq, &data->prox_work,20);
 	}
 	if(als_s){
-		isl29028_enable_prox(data->client);
+		isl29028_enable_als(data->client);
 		queue_delayed_work(wq, &data->als_work,20);
 	}
 	PR_DEB("Exit %s\n",__func__);
